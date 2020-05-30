99a7dae859f2b007b632c2dc0fe2125a38b9e592
Hayden Young <hayden@knowledgearc.com>
2020-05-30 12:50:12 +0800

    Updated Ropsten contract addresses.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,46 +3,40 @@ const Miner = artifacts.require("./Miner.sol");
 const Treasury = artifacts.require("./Treasury.sol");
 const Issuance = artifacts.require("./Issuance.sol");
 
-const Artifactor = require("@truffle/artifactor");
 const mkdirp = require('mkdirp');
+var fs = require('fs');
 
-module.exports = async function(deployer) {
-    await deployer.deploy(Miner);
-
-    const miner = await Miner.deployed();
-
-    await deployer.deploy(Issuance, miner.address);
-
-    const issuance = await Issuance.deployed();
-
-    await deployer.deploy(Treasury, miner.address);
-
-    const treasury = await Treasury.deployed();
-    await miner.setMinter(treasury.address);
-
-    const networksPath = "./networks/"+deployer.network;
+const saveNetworkArtifact = async function(contract, network) {
+    const contractName = contract.constructor._json.contractName;
+    const networksPath = "./networks/"+network;
+    const contractPath = networksPath+"/"+contractName+".json";
 
     mkdirp.sync(networksPath);
 
-    const artifactor = new Artifactor(networksPath);
-
-    const data = {
-        [miner.constructor._json.contractName]: {
-            "contractName": miner.constructor._json.contractName,
-            "address": miner.address,
-            "abi": miner.abi
-        },
-        [issuance.constructor._json.contractName]: {
-            "contractName": issuance.constructor._json.contractName,
-            address: issuance.address,
-            abi: issuance.abi
-        },
-        [treasury.constructor._json.contractName]: {
-            "contractName": treasury.constructor._json.contractName,
-            address: treasury.address,
-            abi: treasury.abi
-        }
+    const artifact = {
+        "contractName": contractName,
+        "address": contract.address,
+        "abi": contract.abi
     }
 
-    await artifactor.saveAll(data);
+    fs.writeFileSync(contractPath, JSON.stringify(artifact, null, 2));
+}
+
+module.exports = async function(deployer) {
+    await deployer.deploy(Miner);
+    const miner = await Miner.deployed();
+
+    saveNetworkArtifact(miner, deployer.network);
+
+    await deployer.deploy(Issuance, miner.address);
+    const issuance = await Issuance.deployed();
+
+    saveNetworkArtifact(issuance, deployer.network);
+
+    await deployer.deploy(Treasury, miner.address);
+    const treasury = await Treasury.deployed();
+
+    saveNetworkArtifact(treasury, deployer.network);
+
+    await miner.setMinter(treasury.address);
 }
