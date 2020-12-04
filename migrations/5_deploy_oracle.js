85332f2ce556d1c709fecb6e63e0de2e6dd14fae
Hayden Young <hayden.y@minertoken.io>
2020-12-04 13:32:47 +0800

    fix: Assign price feed oracle based on current network.

--- migrations/5_deploy_oracle.js
+++ migrations/5_deploy_oracle.js
@@ -8,27 +8,33 @@ const TokenSwap = artifacts.require("./TokenSwap.sol");
 const TestToken = artifacts.require("./TestToken.sol");
 
 module.exports = async function(deployer, network) {
-    let priceFeedETHAddress = process.env.CHAINLINK_PRICE_FEED_ETH;
+    let priceFeedETHAddress = null;
 
     await deployer.deploy(MinerUSDOracle);
     const oracle = await MinerUSDOracle.deployed();
     const issuance = await getIssuance(network);
 
     // if development, deploy the mock price feed.
-    if (network === "development" || network == "test") {
-        await deployer.deploy(PriceFeedETH);
-        const priceFeedEth = await PriceFeedETH.deployed();
-        priceFeedETHAddress = priceFeedEth.address;
+    switch (network) {
+        case "kovan":
+            priceFeedETHAddress = "0x9326BFA02ADD2366b30bacB125260Af641031331";
+            break;
 
-        await deployer.deploy(PriceFeedTestToken);
-        const priceFeedTestToken = await PriceFeedTestToken.deployed();
+        default:
+            await deployer.deploy(PriceFeedETH);
+            const priceFeedEth = await PriceFeedETH.deployed();
+            priceFeedETHAddress = priceFeedEth.address;
 
-        await deployer.deploy(TestToken);
-        const testToken = await TestToken.deployed();
+            await deployer.deploy(PriceFeedTestToken);
+            const priceFeedTestToken = await PriceFeedTestToken.deployed();
 
-        saveNetworkArtifact(priceFeedEth, deployer.network);
-        saveNetworkArtifact(priceFeedTestToken, deployer.network);
-        saveNetworkArtifact(testToken, deployer.network);
+            await deployer.deploy(TestToken);
+            const testToken = await TestToken.deployed();
+
+            saveNetworkArtifact(priceFeedEth, deployer.network);
+            saveNetworkArtifact(priceFeedTestToken, deployer.network);
+            saveNetworkArtifact(testToken, deployer.network);
+            break;
     }
 
     const ethSwap = await deployer.deploy(
