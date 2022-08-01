da3326868682b07c0afe8ebb4e605c98c4ba7750
Hayden Young <hayden.y@minertoken.io>
2022-08-01 22:32:22 +0800

    refactor: Only support solidity versions that do not result in OZ errors.

--- contracts/MinerReserve.sol
+++ contracts/MinerReserve.sol
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity >=0.8.2 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract MinerReserve is AccessControl {
-    using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
     address private _token;
